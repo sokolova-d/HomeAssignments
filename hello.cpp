8adc3beeac99d03039c5bda284aeca31f3f63404
Sokolova Dasha <st141899@student.spbu.ru>
2025-10-22 20:38:28 +0300

    Add new files to git

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include "hello.h"
+
+#include <iostream>
+
+void myproject::sayHello() {
+    std::cout << "Hello, world!" << std::endl;
+    while (true){
+        std::string s;
+        std::cin >> s;
+        std::cout << "Hello, " << s << "!" << std::endl;
+    }
+}
