3e12c77eb4b28f6ca8e8a1f42c8c7b0226571082
Sokolova Dasha <st141899@student.spbu.ru>
2025-11-06 16:00:52 +0300

    Delete extra files

---
+++ Assignment3/TargetSys.cpp
@@ -0,0 +1,14 @@
+/*
+* Sokolova Dasha
+* st141899@student.spbu.ru
+* My project number three.
+*/
+
+#include "TargetSys.h"
+
+TargetSys::TargetSys() {}
+TargetSys::~TargetSys() {}
+
+bool TargetSys::lockTarget() {
+    return true;
+}
